a292ecd6da4f40929e5edf6608dc43a75c5a90bf
markjoho <markojoho@gmail.com>
2022-03-25 18:08:19 -0400

    Make the value inputed in the box appear in the instruction
    The value inputed in the box appear in the instruction, but it's not real time update.

--- js/app.js
+++ js/app.js
@@ -11,12 +11,15 @@ window.onload = () => {
             font-family: inherit;
             border-radius: none`;
 
+
     function showAlertWhiteRice () {
-        document.getElementById("Instruction").innerHTML ="Combine 1 cup of rice with 2 cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.";
+        var HowMuch = document.getElementById(`number-box`).value;
+        document.getElementById("Instruction").innerHTML ="Combine " + HowMuch + " cup of rice with " + HowMuch*2 + " cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.";
     }
 
     function showAlertCaliforniaRice () {
-        document.getElementById("Instruction").innerHTML ="For slightly al dente rice: Combine 1 1/4 cups of rice with 2 cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. Remove from heat and let stand for 5 minutes. Fluff with a fork and serve. For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.";
+        var HowMuch = document.getElementById(`number-box`).value;
+        document.getElementById("Instruction").innerHTML ="For slightly al dente rice: Combine " + HowMuch + " cups of rice with " + HowMuch*1.6 + " cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. Remove from heat and let stand for 5 minutes. Fluff with a fork and serve. For softer rice: Increase liquid by " + HowMuch*0.4 + " cup and cook time by 5 minutes.";
     }
 
     if (null !== leftButton) {
