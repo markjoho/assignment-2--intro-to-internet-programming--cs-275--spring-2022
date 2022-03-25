bf5742a597dd373c6bffb2a991b29138ba897c22
markjoho <98351851+markjoho@users.noreply.github.com>
2022-03-25 14:21:36 -0400

    Created 2 buttons

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 window.onload = () => {
-    alert(`JavaScript Works`);
+    let leftButton = document.getElementById(`white-rice`),
+        rightButton = document.getElementById(`california-rice`),
+        errorStyles = `background-color: red;
+            padding: 2px;
+            color: white;
+            font-family: monospace;
+            border-radius: 4px;`,
+        resetStyles = `background-color: inherit;
+            padding: inherit;
+            font-family: inherit;
+            border-radius: none`;
+
+    function showAlert () {
+        alert(`You clicked the ${this.id}`);
+    }
+
+    if (null !== leftButton) {
+        leftButton.addEventListener(`click`, showAlert);
+    } else {
+        console.error(`A reference to ID %c left-button %c could not be established`, errorStyles, resetStyles);
+    }
+
+    if (null !== rightButton) {
+        rightButton.addEventListener(`click`, showAlert);
+    } else {
+        console.error(`A reference to ID %c right-button %c could not be established`, errorStyles, resetStyles);
+    }
 };
