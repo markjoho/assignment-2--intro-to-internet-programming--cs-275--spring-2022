2625b12b1be8aebd5e3481127633d7f207601204
markjoho <markojoho@gmail.com>
2022-03-25 15:08:20 -0400

    Make instructions text below the number box

--- js/app.js
+++ js/app.js
@@ -11,19 +11,23 @@ window.onload = () => {
             font-family: inherit;
             border-radius: none`;
 
-    function showAlert () {
-        alert(`You clicked the ${this.id}`);
+    function showAlertWhiteRice () {
+        document.getElementById("Instruction").innerHTML ="Combine 1 cup of rice with 2 cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.";
+    }
+
+    function showAlertCaliforniaRice () {
+        document.getElementById("Instruction").innerHTML ="For slightly al dente rice: Combine 1 1/4 cups of rice with 2 cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. Remove from heat and let stand for 5 minutes. Fluff with a fork and serve. For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.";
     }
 
     if (null !== leftButton) {
-        leftButton.addEventListener(`click`, showAlert);
+        leftButton.addEventListener(`click`, showAlertWhiteRice);
     } else {
-        console.error(`A reference to ID %c left-button %c could not be established`, errorStyles, resetStyles);
+        console.error(`A reference to ID %c white-rice %c could not be established`, errorStyles, resetStyles);
     }
 
     if (null !== rightButton) {
-        rightButton.addEventListener(`click`, showAlert);
+        rightButton.addEventListener(`click`, showAlertCaliforniaRice);
     } else {
-        console.error(`A reference to ID %c right-button %c could not be established`, errorStyles, resetStyles);
+        console.error(`A reference to ID %c california-rice %c could not be established`, errorStyles, resetStyles);
     }
 };
