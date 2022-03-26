52c0d3fa1799a0c3ff012daeea4da72cbd6d8582
markjoho <markojoho@gmail.com>
2022-03-25 20:46:29 -0400

    modified HowMuch initiated

--- js/app.js
+++ js/app.js
@@ -12,14 +12,15 @@ window.onload = () => {
             border-radius: none`;
         isWhiteRice = new Boolean(false);
         isCalRice = new Boolean(false);
+        var HowMuch;
 
     function showAlertWhiteRice () {
-        var HowMuch = document.getElementById(`number-box`).value;
+        HowMuch = document.getElementById(`number-box`).value;
         document.getElementById("Instruction").innerHTML ="Combine " + HowMuch + " cup of rice with " + HowMuch*2 + " cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.";
     }
 
     function showAlertCaliforniaRice () {
-        var HowMuch = document.getElementById(`number-box`).value;
+        HowMuch = document.getElementById(`number-box`).value;
         document.getElementById("Instruction").innerHTML ="For slightly al dente rice: Combine " + HowMuch + " cups of rice with " + HowMuch*1.6 + " cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. Remove from heat and let stand for 5 minutes. Fluff with a fork and serve. For softer rice: Increase liquid by " + HowMuch*0.4 + " cup and cook time by 5 minutes.";
     }
 
